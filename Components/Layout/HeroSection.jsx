52e692c79f88d9f1cacbe2c409130d3bc89a3e26
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-01-01 14:51:19 +0600

    Add Hero Section

--- Components/Layout/HeroSection.jsx
+++ Components/Layout/HeroSection.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-
+import Slider from "react-slick";
+import { FaStar } from "react-icons/fa";
 const HeroSection = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 5000,
+    // cssEase: "linear",
+  };
   return (
     <section id='hero__section'>
       <Container>
@@ -60,9 +71,70 @@ const HeroSection = () => {
               </div>
             </div>
           </Col>
-          <Col lg={6} className='rightSide'></Col>
+          <Col lg={6} className='rightSide position-relative'>
+            <div>
+              <div className='imgFile '>
+                <img
+                  className='img-fluid'
+                  src='/Images/person/person_1.png'
+                  alt=''
+                />
+              </div>
+              <div className='shape_2 position-absolute'>
+                <img
+                  className='img-fluid'
+                  src='/Images/shape/shape_6.png'
+                  alt=''
+                />
+              </div>
+              <div className='slider__body shadow-md'>
+                <div className='wrapper'>
+                  <Slider {...settings}>
+                    {[...Array(3)].map((item, index) => (
+                      <div className='slider__bodyInner d-flex'>
+                        <div className='d-flex'>
+                          <div className='imgFile'>
+                            <img
+                              className='img-fluid'
+                              src='/Images/products/product_1.png'
+                              alt=''
+                            />
+                          </div>
+                          <div className='textFile pl--20 d-flex align-items-center'>
+                            <div>
+                              <div className='title__box'>
+                                <h2>Denim Jeans</h2>
+                                <p>Best Seller</p>
+                              </div>
+                              <div className='d-flex mt--10 align-items-center'>
+                                <div className='price'>
+                                  <span>$9.5</span>
+                                </div>
+                                <div className='review ml--8'>
+                                  {[...Array(5)].map((item, index) => (
+                                    <span key={index} className='mr--3'>
+                                      <FaStar />
+                                    </span>
+                                  ))}
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </Slider>
+                </div>
+              </div>
+            </div>
+          </Col>
         </Row>
       </Container>
+      {/* <div className='shapeImage position-absolute'>
+        <div className='shape_1'>
+          <img src='/Images/shape/shape_4.png' alt='' />
+        </div>
+      </div> */}
     </section>
   );
 };
