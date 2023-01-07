380d28c701d200052185328e628ffe0e0631bcb7
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-01-07 18:27:18 +0600

    Add single product page

--- Components/ChildComponents/SingleProductSection.jsx
+++ Components/ChildComponents/SingleProductSection.jsx
@@ -17,7 +17,6 @@ import {
   FaTwitter,
 } from "react-icons/fa";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import FeaturedProduct from "./FeaturedProduct";
 const SingleProductSection = () => {
   const [item, setItem] = useState(1);
   const increaseValue = () => {
@@ -334,10 +333,8 @@ const SingleProductSection = () => {
                     <TabList>
                       <Tab>Description</Tab>
                       <Tab>Reviews (200)</Tab>
-                      <Tab>Discussion</Tab>
-                      <Tab>Gift Cards</Tab>
+                      <Tab>Shipping and Returns</Tab>
                     </TabList>
-
                     {/* Description Body Section */}
                     <TabPanel>
                       <Row>
@@ -572,6 +569,80 @@ const SingleProductSection = () => {
                                     fermentum aliquam ultrices massa.
                                   </p>
                                 </div>
+                              </div>
+                            </div>
+                          </div>
+                        </Col>
+                        <Col lg={4}></Col>
+                      </Row>
+                    </TabPanel>
+                    {/* Shipping and Returns Body Section */}
+                    <TabPanel>
+                      <Row>
+                        <Col lg={8}>
+                          <div className='shipping__returns__section'>
+                            <div className='wrapper__inner'>
+                              <div>
+                                <h2>Returns Policy</h2>
+                                <p>
+                                  You may return most new, unopened items within
+                                  30 days of delivery for a full refund. We'll
+                                  also pay the return shipping costs if the
+                                  return is a result of our error (you received
+                                  an incorrect or defective item, etc.).
+                                </p>
+                                <br />
+                                <p>
+                                  You should expect to receive your refund
+                                  within four weeks of giving your package to
+                                  the return shipper, however, in many cases you
+                                  will receive a refund more quickly. This time
+                                  period includes the transit time for us to
+                                  receive your return from the shipper (5 to 10
+                                  business days), the time it takes us to
+                                  process your return once we receive it (3 to 5
+                                  business days), and the time it takes your
+                                  bank to process our refund request (5 to 10
+                                  business days).
+                                </p>
+                                <br />
+                                <p>
+                                  If you need to return an item, simply login to
+                                  your account, view the order using the
+                                  'Complete Orders' link under the My Account
+                                  menu and click the Return Item button. We'll
+                                  notify you via e-mail of your refund once
+                                  we've received and processed the returned
+                                  item.
+                                </p>
+                              </div>
+                              <div className='mt--30'>
+                                <h2>Shipping Policy</h2>
+                                <p>
+                                  We can ship to virtually any address in the
+                                  world. Note that there are restrictions on
+                                  some products, and some products cannot be
+                                  shipped to international destinations.
+                                </p>
+                                <br />
+                                <p>
+                                  When you place an order, we will estimate
+                                  shipping and delivery dates for you based on
+                                  the availability of your items and the
+                                  shipping options you choose. Depending on the
+                                  shipping provider you choose, shipping date
+                                  estimates may appear on the shipping quotes
+                                  page.
+                                </p>
+                                <br />
+                                <p>
+                                  Please also note that the shipping rates for
+                                  many items we sell are weight-based. The
+                                  weight of any such item can be found on its
+                                  detail page. To reflect the policies of the
+                                  shipping companies we use, all weights will be
+                                  rounded up to the next full pound.
+                                </p>
                               </div>
                             </div>
                           </div>
